50b68acaaaa5e3fa3314f5ba618a20ca253f4eed
Stephan <stephanryer@hotmail.com>
2023-12-19 12:52:50 +0100

    hey

--- setup.sh
+++ setup.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
-create_symlink ()
-{
-	symlink_file=$1
-	source_vimrc=$2
-
-	ensure_file_not_exists $symlink_file;
-	echo "Creating symlink $symlink_file > $source_vimrc" 
-	ln -s "$source_vimrc" "$symlink_file"
-}
-
-ensure_file_not_exists()
-{
-	filename=$1
-	if test -f "$filename"; then
-		echo "$filename already exists. Deleting the file..."
-		rm "$filename"
-	fi
-}
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SOURCE_VIM_FILE="$SCRIPT_DIR/.vimrc"
-create_symlink "$SOURCE_VIM_FILE""~/.vimrc" 
-create_symlink "$SOURCE_VIM_FILE" "~/.ideavimrc"
+ln -s "$SOURCE_VIM_FILE" ~/.vimrc
+ln -s "$SOURCE_VIM_FILE" ~/.ideavimrc
 
 # Telling nvim to use ~/.vimrc
 INIT_CONTENT="source $SOURCE_VIM_FILE"
